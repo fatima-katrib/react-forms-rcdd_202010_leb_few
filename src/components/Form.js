74c3be63a0990630fd9cdd738b888293846b47b0
jovial-application-1943 <fatima.katrib@lau.edu>
2021-03-20 14:12:09 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,7 @@ class Form extends React.Component {
       <form>
         <DisplayData formData={this.state} />
         <input type="text" name="firstName" onChange={event =>  this.props.handleChange(event)} value={this.props.formData.firstName} />
-        <input type="text" name="lastName" onChange={event => this.props.handleChange(event)} value={his.props.formData.lastName} />
+        <input type="text" name="lastName" onChange={event => this.props.handleChange(event)} value={this.props.formData.lastName} />
       </form>
     )
   }
