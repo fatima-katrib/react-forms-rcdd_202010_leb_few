dc4cdde14394c01f291db6a4eebefb8a9ab09fc7
jovial-application-1943 <fatima.katrib@lau.edu>
2021-03-20 14:07:25 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,23 +2,7 @@ import React from 'react';
 import DisplayData from "./src/components/displayData.js";
 
 class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
-  }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
-
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
-  
 
   render() {
     return (
